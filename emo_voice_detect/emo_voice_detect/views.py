a33083f2fbfe3584f34743114452710add88ad14
Parth Sangoi <parthsangoi1@gmail.com>
2023-10-03 22:28:20 +0530

    base frontend

---
+++ emo_voice_detect/emo_voice_detect/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def camera_input(request):
+    # Your view logic goes here
+    return render(request, 'camera.html')
+def home(request):
+    # Add any context data if needed
+    return render(request, 'base.html')
