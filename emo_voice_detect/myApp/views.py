d90dc903acdcf48818838e45d2e1976ba472c48c
Hawk2272 <gaurav.chawla@somaiya.edu>
2023-10-08 15:14:07 +0530

    login/signup backend

--- emo_voice_detect/myApp/views.py
+++ emo_voice_detect/myApp/views.py
@@ -12,7 +12,14 @@ def home(request):
     return render(request, 'base.html')
 
 def login(request):
-    return render(request, 'index.html')
+    form=UserCreationForm()
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save
+    context = {'form':form}
+    return render(request,'index.html',context)
+
 
 def camera_input(request):
     return render(request, 'camera.html')
@@ -20,46 +27,45 @@ def camera_input(request):
 def analyze_emotion(request):
     return render(request, 'analyze_emotion.html')
 
-
-class CustomLoginView(LoginView):
-    template_name = 'index.html'
-
 class CustomSignupView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     template_name = 'index.html'
     success_url = '/login/'
 
-@csrf_exempt
-def detect_emotion(request):
-    if request.method == 'POST':
-        image_data = request.POST.get('image_data')
+class CustomLoginView(LoginView):
+    template_name = 'index.html'
+    
+# @csrf_exempt
+# def detect_emotion(request):
+#     if request.method == 'POST':
+#         image_data = request.POST.get('image_data')
 
-        # Initialize the Google Cloud Vision API client
-        client = vision.ImageAnnotatorClient()
+#         # Initialize the Google Cloud Vision API client
+#         client = vision.ImageAnnotatorClient()
 
-        # Convert the base64 image data to bytes
-        image_bytes = base64.b64decode(image_data.split(',')[1])
+#         # Convert the base64 image data to bytes
+#         image_bytes = base64.b64decode(image_data.split(',')[1])
 
-        # Create an image object
-        image = vision.Image(content=image_bytes)
+#         # Create an image object
+#         image = vision.Image(content=image_bytes)
 
-        # Detect faces in the image
-        response = client.face_detection(image=image)
+#         # Detect faces in the image
+#         response = client.face_detection(image=image)
 
-        # Get emotion data (e.g., happiness score)
-        face = response.face_annotations[0]  # Assuming only one face is detected
-        emotions = {
-            "joy": face.joy_likelihood,
-            "sorrow": face.sorrow_likelihood,
-            "anger": face.anger_likelihood,
-            "surprise": face.surprise_likelihood,
-            "under_exposed": face.under_exposed_likelihood,
-            "blurred": face.blurred_likelihood,
-            "headwear": face.headwear_likelihood,
-        }
+#         # Get emotion data (e.g., happiness score)
+#         face = response.face_annotations[0]  # Assuming only one face is detected
+#         emotions = {
+#             "joy": face.joy_likelihood,
+#             "sorrow": face.sorrow_likelihood,
+#             "anger": face.anger_likelihood,
+#             "surprise": face.surprise_likelihood,
+#             "under_exposed": face.under_exposed_likelihood,
+#             "blurred": face.blurred_likelihood,
+#             "headwear": face.headwear_likelihood,
+#         }
 
-        # Determine the detected emotion
-        detected_emotion = max(emotions, key=emotions.get)
+#         # Determine the detected emotion
+#         detected_emotion  = max(emotions, key=emotions.get)
 
-        return JsonResponse({'emotion': detected_emotion})
-    return JsonResponse({'error': 'Invalid request method'})
+#         return JsonResponse({'emotion': detected_emotion})
+#     return JsonResponse({'error': 'Invalid request method'})
